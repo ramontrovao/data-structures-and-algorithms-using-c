23eaf70278bccebd7d435f6a6a5ea55f7a4c1f5b
ramon trovão <ramonzinhg12@gmail.com>
2025-02-20 23:07:13 -0300

    test

--- pointers/pointer_of_pointers/pointer_of_pointers.c
+++ pointers/pointer_of_pointers/pointer_of_pointers.c
@@ -14,7 +14,6 @@ int main() {
 
   printf("n = %d, &n = %p \n", n, &n);
   printf("p1 = %p, &p1 = %p \n", p1, &p1);
-  printf("p2 = %p, &p2 = %p, *p2 = %p, **p2 = %d \n", p2, &p2, *p2, **p2);
 
   return 0;
 }
