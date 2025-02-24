b3ba3b3c1d208f869a9a02d967838f3cf46b3d18
ramon trovão <ramonzinhg12@gmail.com>
2025-02-24 07:56:21 -0300

    feat: add basic_function.c

--- functions/basic_function/basic_function.c
+++ functions/basic_function/basic_function.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+int sum(int x, int y) {
+  printf("x = %d, &x = %p\n", x, &x);
+  printf("y = %d, &y = %p\n", y, &y);
+
+  int z = x + y;
+
+  printf("z = %d, &z = %p\n", z, &z);
+
+  return z;
+}
+
 int main() {
-  printf("Teste");
+  int a = 5;
+  int b = 10;
+  int c;
+
+  printf("a = %d, &a = %p\n", a, &a);
+  printf("b = %d, &b = %p\n", b, &b);
+
+  c = sum(a, b);
+
+  printf("c = %d, &c = %p\n", c, &c);
 
   return 0;
 }
